e732ff9e8e4770abc3c21b37b383f989a5fa1091
santiago hurtado <sahurtaro@gmail.com>
2024-10-31 16:43:27 -0500

    Feature: books services. GetAll and Insert

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BooksController } from './modules/books/books.controller';
+import { BooksService } from './modules/books/books.service';
 
 @Module({
   imports: [],
   controllers: [AppController, BooksController],
-  providers: [AppService],
+  providers: [AppService, BooksService],
 })
 export class AppModule {}
